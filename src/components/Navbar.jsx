fc5fca839ffc91412f2253d7ac440e741ff154bd
roy9 <roymurimi527@gmail.com>
2026-07-03 12:18:02 +0300

    Set up frontend structure and routing

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link> |{" "}
+      <Link to="/about">About</Link> |{" "}
+      <Link to="/services">Services</Link> |{" "}
+      <Link to="/contact">Contact</Link> |{" "}
+      <Link to="/login">Login</Link> |{" "}
+      <Link to="/register">Register</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/ResetPassword.jsx
@@ -0,0 +1,5 @@
+function ResetPassword() {
+  return <h1>Reset Password Page</h1>;
+}
+
+export default ResetPassword;
